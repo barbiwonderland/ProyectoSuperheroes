400f3c7cada0fd5788727624fbc5964d7743f3e3
barbiwonderland <barbiiee.8@gmail.com>
2021-05-15 00:12:15 -0300

    equipo

--- src/components/equipo.js
+++ src/components/equipo.js
@@ -1,38 +1,42 @@
 import React, { useEffect, useContext, useState } from "react";
 import axios from "axios";
-import userEventContext from '../userEventContext';
-
-
+import userEventContext from "../userEventContext";
 
 export default function Equipo() {
-  const {idTeam} =useContext(userEventContext);
-  console.log(idTeam);
-// let id_Team = Object.values(idTeam);
-idTeam.map((x)=>{
-console.log(`https://superheroapi.com/api/2831945550360412/${x}`);
-let url= `https://superheroapi.com/api/2831945550360412/${x}`;
-  function CalltoId(){
-    axios
-    .get(
-     {url}
-    )
-    .then((res) => {
-      console.log(res);
-      
-       
-    })
-    .catch((error) => {
-     console.log(error);
-    })
-  }
-  CalltoId();
-})
+  const [equipoPersonajes, setequipoPersonajes] = useState([]);
+  let personajesId = [];
+  personajesId = JSON.parse(localStorage.getItem("id"));
 
-
+  const Teamurl = ` https://secret-ocean-49799.herokuapp.com/https://superheroapi.com/api/2831945550360412/${personajesId[0]}`;
+  useEffect(() => {
+    const ObtenerEquipo = () => {
+      axios
+        .get(Teamurl)
+        .then((res) => {
+          console.log(res.data);
+          setequipoPersonajes(res.data);
+        })
+        .catch(() => {
+          console.log("bad");
+          //  window.location.reload();
+        });
+    };
+    ObtenerEquipo();
+  }, [Teamurl]);
+  const { name, image } = equipoPersonajes;
   return (
- <React.Fragment>
-   <h1 className="text-center">Equipo</h1>
-
- </React.Fragment>
-  )
+    <React.Fragment>
+      <h1 className="text-center my-3">Equipo</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 text-center">
+            <h4 >{name}</h4>
+           <img src={image.url} width="150" alt="" />
+           <br />
+            <button className=" mt-3 btn btn-danger">Eliminar</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,31 +1,39 @@
 import React, { Component,setState,useState
  } from 'react'
  import "./styles/form.css";
+ import { setUserSession } from '../common';
+ import axios from "axios";
 
-export default function Login() {
-
+function Login(props) {
+  
     return (
         <React.Fragment>
-        <div className="wrapper fadeInDown">
+            <div className="mt-5">
+        <div className="wrapper fadeInDown   ">
         <div id="formContent">
        
-          <div className="fadeIn first">
+          <div className=" fadeIn first">
               <h5>SuperHero</h5>
           </div>
       
     <form action="">
  
-            <input type="text" id="login" className="fadeIn second" name="login" placeholder="login"/>
-            <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"/>
+            <input type="text"  id="login" className="fadeIn second" name="login" placeholder="Usuario"/>
+            <input type="text" id="password" className="fadeIn third" name="login" placeholder="Contraseña"/>
             <input type="submit" className="fadeIn fourth" value="Log In"/>
             </form>
         
           <div id="formFooter">
-            <a className="underlineHover" href="#">Forgot Password?</a>
+            {/* <a className="underlineHover" href="#">Forgot Password?</a> */}
           </div>
       
         </div>
       </div>
+      </div>
       </React.Fragment>
-    )
-}
+    );
+    }
+
+
+
+export default Login;
